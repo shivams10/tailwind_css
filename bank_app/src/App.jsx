ac786f7e1aeaa69cf25bf0d5e5b48abf8f7a7c23
Shivam Shukla <shivam@gkmit.co>
2022-11-14 12:40:21 +0530

    added styles.js and created basic code for app.js

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-
+import styles from "./style";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      Hello 
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+       
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Buisness
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA 
+          Footer
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
